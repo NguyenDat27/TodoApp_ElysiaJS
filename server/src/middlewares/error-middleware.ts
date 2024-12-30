c5c12e23361c6de7f3c6b6a5989d36c895438c81
Thanh Dat <caitlyn.ochamp@gmail.com>
2024-12-30 18:54:56 +0700

    fix: change code error

--- server/src/middlewares/error-middleware.ts
+++ server/src/middlewares/error-middleware.ts
@@ -2,11 +2,11 @@ import Elysia from 'elysia';
 import { ERROR_MSG } from 'lib/constant';
 
 export const errorMiddleware = (app: Elysia) =>
-    app.onError(({ error, set }) => {
+    app.onError(({ error, code }) => {
+        
         console.error('Global Error:', error);
-
-        set.status = 500;
         return {
+            code: code,
             message: ERROR_MSG.SOMETHING_WENT_WRONG,
             data: null,
         };
